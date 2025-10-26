312301ac2a9e99036e01a05bb42558eb8e61216d
Felipe <ferrocde@gmail.com>
2025-10-26 17:45:25 -0300

    fix: corrige alias @ no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Importante para funcionar na Vercel
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "src"), // Corrige o alias @
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
-  base: '/',
-})
+});
